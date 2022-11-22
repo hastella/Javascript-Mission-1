ecbd4d8d182f63f7a2ec22b401f4279b31146b4d
Stella_Ha <sangyeeh@gmail.com>
2022-11-22 17:28:37 +0900

    toggle local storage 추가

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // do something!
+const nav = document.querySelector("nav");
+const navBtn = document.querySelector(".toggle");
+const hiddenBody = document.querySelector("body");
+
+window.addEventListener("load", () => body.classList.remove(".preload"));
+window.addEventListener("load", () => body.classList.add("body"));
+
+navBtn.onclick = () => nav.classList.toggle("active");
+
+navBtn.addEventListener("click", function () {
+  if (nav.classList.contains("active")) {
+    localStorage.setItem("menu-state", "open");
+    console.log(localStorage);
+  } else {
+    localStorage.setItem("menu-state", "closed");
+  }
+});
+if (localStorage.getItem("menu-state") === "open") {
+  nav.classList.toggle("active");
+}
