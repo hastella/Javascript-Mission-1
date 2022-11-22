248c33e7bc3accad42b193faf51b77490db5069f
Stella_Ha <sangyeeh@gmail.com>
2022-11-23 01:02:52 +0900

    렌더링시 불필요한 트랜지션 방지 구현 완료

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-// do something!
 const nav = document.querySelector("nav");
 const navBtn = document.querySelector(".toggle");
 const hiddenBody = document.querySelector("body");
 
-window.addEventListener("load", () => body.classList.remove(".preload"));
-window.addEventListener("load", () => body.classList.add("body"));
-
 navBtn.onclick = () => nav.classList.toggle("active");
 
+// 토글 active 상태 locastorage 저장
 navBtn.addEventListener("click", function () {
   if (nav.classList.contains("active")) {
     localStorage.setItem("menu-state", "open");
@@ -18,4 +15,15 @@ navBtn.addEventListener("click", function () {
 });
 if (localStorage.getItem("menu-state") === "open") {
   nav.classList.toggle("active");
+  window.addEventListener("load", () => body.classList.add("body"));
 }
+
+// 초기 렌더링 시에 불필요한 트랜지션 방지
+window.addEventListener(
+  "load",
+  function load() {
+    window.removeEventListener("load", load, false);
+    document.body.classList.remove("preload");
+  },
+  false
+);
